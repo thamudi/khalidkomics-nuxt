6c4cc3c434d192b77b7d51f9ac483f011d09a5c2
thamudi <tamim.hamoudi@gmail.com>
2023-06-09 17:45:08 +0300

    feat: add homepage slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="container">
+    <div v-if="!pending && !error">
+      <ComicSlider :comics="comicsData" />
+      <ComicPagination
+        :comics-pagination="comicsPaginationData"
+        @change-comic="setPage"
+      />
+    </div>
+    <div v-else>
+      <ComicLoader />
+    </div>
   </div>
 </template>
+
+<script setup>
+  import qs from 'qs'
+  import { get, set } from '@vueuse/core'
+  const { locale } = useI18n()
+  const currentPage = useState('comicCurrentPage', () => 1)
+
+  const setPage = (newPage) => {
+    if (newPage <= get(comicsPaginationData).total) set(currentPage, newPage)
+  }
+
+  const query = computed(() =>
+    qs.stringify(
+      {
+        populate: '*',
+        sort: ['releaseDate:desc'],
+        pagination: {
+          page: get(currentPage),
+          pageSize: 1,
+        },
+        locale: get(locale),
+      },
+      {
+        encodeValuesOnly: true,
+      }
+    )
+  )
+
+  const {
+    data: comics,
+    error,
+    pending,
+  } = await useAsyncData(
+    'comics',
+    () => {
+      return $cms(`comics?${get(query)}`, { cache: true })
+    },
+    {
+      server: false,
+      watch: [query],
+    }
+  )
+
+  const comicsData = computed(() => get(comics).data[0].attributes)
+
+  const comicsPaginationData = computed(() => {
+    return {
+      page: get(comics).meta.pagination.page,
+      pageCount: get(comics).meta.pagination.pageCount,
+      total: get(comics).meta.pagination.total,
+    }
+  })
+</script>
