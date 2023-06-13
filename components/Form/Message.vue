0db1e461d1349389c0f5491bd35a04e03825d61b
thamudi <tamim.hamoudi@gmail.com>
2023-06-13 15:15:04 +0300

    refactor: replace message popup with a modal

--- components/Form/Message.vue
+++ components/Form/Message.vue
@@ -1,27 +1,33 @@
 <template>
-  <div :class="`flex flex-col justify-start border-2 p-4 mx-auto mt-4 ${type}`">
-    <div class="flex flex-col">
-      <div class="flex flex-row justify-between items-center mb-2">
-        <div class="flex flex-col">
-          <p class="font-bold first-letter:uppercase">{{ type }}</p>
-
-          <p>{{ t(message) }}</p>
-        </div>
-        <div class="flex flex-row gap-x-4 align-top">
-          <nuxt-img
-            :src="`/images/contact/${type}.svg`"
-            :alt="`${type}`"
-            width="120"
-            height="120"
-          />
-          <i class="cursor-pointer text-2xl" @click=""> x </i>
-        </div>
+  <dialog
+    ref="dialog"
+    :class="`${type} p-8 rounded-[40px] border-4 border-kk-blue-dark max-w-[50%] w-full`"
+  >
+    <div class="flex flex-row justify-between items-center mb-2">
+      <p>{{ t(message) }}</p>
+      <div class="flex flex-row gap-x-4 align-top">
+        <nuxt-img
+          :src="`/images/contact/${type}.svg`"
+          :alt="`${type}`"
+          width="120"
+          height="120"
+        />
       </div>
     </div>
-  </div>
+    <form method="dialog">
+      <button
+        @click="$emit('closeDialog')"
+        class="my-4 px-4 border-2 solid border-wi border-kk-black hover:bg-kk-blue hover:text-kk-blue-light"
+      >
+        {{ t('ok') }}
+      </button>
+    </form>
+  </dialog>
 </template>
 <script setup>
+  defineEmits(['closeDialog'])
   const { t } = useI18n()
+  const dialog = ref(null)
   const props = defineProps({
     type: {
       type: String,
@@ -32,20 +38,21 @@
       default: '',
     },
   })
+  onMounted(() => dialog.value.showModal())
 </script>
 <style scoped lang="postcss">
   .success {
-    background-color: rgba(109, 185, 129, 0.1);
+    /* background-color: rgba(109, 185, 129); */
     border-color: #6db981;
     color: #6db981;
   }
   .error {
-    background-color: rgba(242, 111, 84, 0.1);
+    /* background-color: rgba(242, 111, 84); */
     border-color: #f26f54;
     color: #f26f54;
   }
   .info {
-    background-color: rgba(11, 150, 171, 0.1);
+    /* background-color: rgba(11, 150, 171); */
     @apply border-kk-blue;
     color: #0b96ab;
   }
